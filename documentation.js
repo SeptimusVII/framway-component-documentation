17885e54abedb257e3128cd6c3423f38dc0a0471
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-11 15:27:54 +0100

    Initial commit

---
+++ documentation.js
@@ -0,0 +1,11 @@
+var Documentation = Object.getPrototypeOf(app).Documentation = new Component("documentation");
+Documentation.debug = true;
+Documentation.createdAt      = "2.0.0";
+Documentation.lastUpdate     = "2.0.0";
+Documentation.version        = "1";
+// Documentation.factoryExclude = true;
+// Documentation.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// Documentation.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
