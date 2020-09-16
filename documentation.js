109e4c5d5ab18f020dcb872b98743d0486bbe3b4
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-16 15:37:13 +0200

    add documentation for buttons, backgrounds and borders

--- documentation.js
+++ documentation.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 	Documentation.factoryExclude = true;
 	// Documentation.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-	var arrManuals = ['flex','grid','opacity','margin-padding'];
+	var arrManuals = ['buttons','backgrounds','borders','margin-padding','flex','grid','opacity'];
 	if(app.components.includes('modalFW'))
 	    arrManuals.push('modalFW');
 	Documentation.prototype.onCreate = function(){
