ba2cbe9c032c16e2e97a02bd25e410b99c84e33a
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-16 16:24:18 +0200

    add images documentation

--- documentation.js
+++ documentation.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 	Documentation.factoryExclude = true;
 	// Documentation.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-	var arrManuals = ['buttons','backgrounds','borders','margin-padding','flex','grid','opacity'];
+	var arrManuals = ['buttons','backgrounds','borders','margin-padding','flex','grid','opacity','images'];
 	if(app.components.includes('modalFW'))
 	    arrManuals.push('modalFW');
 	Documentation.prototype.onCreate = function(){
